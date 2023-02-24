9b480c4d5bfde113ede6af6c22a6ce07bc2a7d2c
arimurashoi2000 <arimura@craid-inc.com>
2023-02-24 10:08:11 +0900

    putTogetherの修正

--- putTogether.php
+++ putTogether.php
@@ -6,10 +6,9 @@ $newTaskDelete = filter_input(INPUT_POST, 'delete');
 $newTaskTitle = filter_input(INPUT_POST, 'title');
 $newTaskNum = filter_input(INPUT_GET, 'ID');
 $limit = 20;
-
-$newTaskContents = htmlspecialchars($NewTaskContents, ENT_QUOTES, 'UTF-8');
-$newTaskTitle = htmlspecialchars($NewTaskTitle, ENT_QUOTES, 'UTF-8');
-$newTaskEdit = htmlspecialchars($NewTaskEdit, ENT_QUOTES, 'UTF-8');
-$newTaskDelete = htmlspecialchars($NewTaskDelete, ENT_QUOTES, 'UTF-8');
-$newTaskContents = htmlspecialchars($NewTaskContents, ENT_QUOTES, 'UTF-8');
-$newTaskNum = htmlspecialchars($NewTaskNum, ENT_QUOTES, 'UTF-8');
+$newTaskContents = htmlspecialchars($newTaskContents, ENT_QUOTES, 'UTF-8');
+$newTaskTitle = htmlspecialchars($newTaskTitle, ENT_QUOTES, 'UTF-8');
+$newTaskEdit = htmlspecialchars($newTaskEdit, ENT_QUOTES, 'UTF-8');
+$newTaskDelete = htmlspecialchars($newTaskDelete, ENT_QUOTES, 'UTF-8');
+$newTaskContents = htmlspecialchars($newTaskContents, ENT_QUOTES, 'UTF-8');
+$newTaskNum = htmlspecialchars($newTaskNum, ENT_QUOTES, 'UTF-8');
